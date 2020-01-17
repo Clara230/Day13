419eff0d5f3088589f22197b876a5feb13e9b609
clara9621 <58587888+clara9621@users.noreply.github.com>
2020-01-17 17:18:54 +0200

    Feature : Added Day13

---
+++ City/CityModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Cities
+{
+    public class CityModelImport
+
+    {
+        public string City_name { get; set; }
+        public string City_ascii { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public string Country { get; set; }
+        public string ISO2 { get; set; }
+        public string ISO3 { get; set; }
+        public string Admin_name { get; set; }
+        public string Capital { get; set; }
+        public double Population { get; set; }
+        public float Id { get; set; }
+    }
+       
+    public class CitiesContext : DbContext
+    {
+        public DbSet<CountryEntity> Countries { get; set; }
+        public DbSet<CityEntity> Cities { get; set; }
+    }
+}
